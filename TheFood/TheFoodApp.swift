9b40cd8f34c4e7d5dbe532e734679a08e4c2cebf
jotkafomat <k-j@xl.wp.pl>
2021-05-24 14:10:59 +0100

    Initial Commit

---
+++ TheFood/TheFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheFoodApp.swift
+//  TheFood
+//
+//  Created by Krzysztof Jankowski on 24/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct TheFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
