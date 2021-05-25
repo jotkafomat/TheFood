4e0d8bf1a4563199d9d98a6c03163748b225dcbf
jotkafomat <k-j@xl.wp.pl>
2021-05-25 12:27:10 +0100

    add RecipeView

---
+++ TheFood/View/RecipeView.swift
@@ -0,0 +1,55 @@
+//
+//  RecipeView.swift
+//  TheFood
+//
+//  Created by Krzysztof Jankowski on 25/05/2021.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+
+    let recipe: Recipe
+    let frameColor: Color = .purple
+
+    var body: some View {
+        Rectangle()
+            .aspectRatio(0.97, contentMode: .fit)
+            .foregroundColor(frameColor)
+            .overlay(
+                Image(decorative: recipe.thumbnail)
+                    .resizable()
+                    .scaledToFill()
+                    .aspectRatio(1, contentMode: .fit)
+                    .clipShape(Circle())
+                    .padding(4)
+            )
+            .overlay(
+                Text(recipe.headline)
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .padding(.bottom, 4),
+                alignment: .bottomLeading
+            )
+    }
+}
+
+struct RecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            RecipeView(recipe: Recipe(
+                        headline: "Thomasina Miers’ recipe for courgetti carbonara",
+                        thumbnail: "carbonara"))
+                .preferredColorScheme(.light)
+                .previewDevice("iPhone 12 Pro Max")
+            RecipeView(recipe: Recipe(
+                        headline: "Thomasina Miers’ recipe for courgetti carbonara",
+                        thumbnail: "carbonara"))
+                .preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .accessibilityMedium)
+                .previewDevice("iPhone 12 Pro Max")
+        }
+    }
+}
