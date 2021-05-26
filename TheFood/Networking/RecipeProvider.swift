d963a7dbd048979fcb6230f6131cb0fdbd00d7a6
jotkafomat <k-j@xl.wp.pl>
2021-05-26 14:25:45 +0100

    add recipeProvider protocol

---
+++ TheFood/Networking/RecipeProvider.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeProvider.swift
+//  TheFood
+//
+//  Created by Krzysztof Jankowski on 26/05/2021.
+//
+
+import Foundation
+import Combine
+
+protocol RecipeProvider {
+    func getLatestRecipes() -> AnyPublisher<[Recipe], Never>
+    
+}
