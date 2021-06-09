138ce37835f24ef612ffa3fad09b314ac1e2a3d9
jotkafomat <k-j@xl.wp.pl>
2021-06-09 12:56:36 +0100

    add preview data

--- TheFoodTests/MockRecipesPublisher.swift
+++ TheFoodTests/MockRecipesPublisher.swift
@@ -18,13 +18,8 @@ enum MockRecipesPublisher: RecipesPublisher {
     func getLatestRecipes() -> AnyPublisher<[Recipe], Never> {
         switch self {
         case .success:
-            return Just([
-            Recipe(headline: "Thomasina Miers’ recipe for courgetti carbonara", thumbnail: "carbonara"),
-            Recipe(headline: "Rachel Roddy’s recipe for lamb and peas", thumbnail: "lambAndPeas"),
-            Recipe(headline: "Birthday custard sponge by Nigella Lawson", thumbnail: "custardSponge"),
-            Recipe(headline: "Stout and chocolate cake with toasted oats by Claire Thomson", thumbnail: "stoutCake"),
-            Recipe(headline: "Simple banana cake by Ravneet Gill", thumbnail: "bananaCake")])
-                .eraseToAnyPublisher()
+            return Just(Recipe.recipes)
+            .eraseToAnyPublisher()
         case .failure:
             return Just<[Recipe]>([]).eraseToAnyPublisher()
         case .any:
