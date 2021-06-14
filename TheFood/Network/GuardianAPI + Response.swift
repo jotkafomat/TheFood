671e2fa4a539ba55f8afb559c7307e9999089dda
jotkafomat <k-j@xl.wp.pl>
2021-06-14 16:39:21 +0100

    add pageSize, currentPage, pages

--- TheFood/Network/GuardianAPI + Response.swift
+++ TheFood/Network/GuardianAPI + Response.swift
@@ -10,6 +10,9 @@ import Foundation
 extension GuardianAPI {
     struct Response {
         let results: [Recipe]
+        let pageSize: Int
+        let currentPage: Int
+        let pages: Int
     }
 }
 extension GuardianAPI.Response: Decodable {
@@ -18,12 +21,15 @@ extension GuardianAPI.Response: Decodable {
         case response
     }
     enum ResponseKeys: String, CodingKey {
-        case results
+        case results, pageSize, currentPage, pages
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let responseContainer = try container.nestedContainer(keyedBy: ResponseKeys.self, forKey: .response)
         results = try responseContainer.decode([Recipe].self, forKey: .results)
+        pageSize = try responseContainer.decode(Int.self, forKey: .pageSize)
+        currentPage = try responseContainer.decode(Int.self, forKey: .currentPage)
+        pages = try responseContainer.decode(Int.self, forKey: .pages)
     }
 }
