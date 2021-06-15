c7c51b6fbf087beeeebdfcd11773439f10887572
jotkafomat <k-j@xl.wp.pl>
2021-06-15 09:48:55 +0100

    add loadMoreRecipes to recipesController

--- TheFood/ViewModel/RecipesController.swift
+++ TheFood/ViewModel/RecipesController.swift
@@ -13,6 +13,9 @@ class RecipesController: ObservableObject {
     @Published private (set) var recipes = [RecipeViewModel]()
     private let recipesPublisher: RecipesPublisher
 
+    private var cancellable = Set<AnyCancellable>()
+    var currentPage: Int
+
     init(recipesPublisher: RecipesPublisher = GuardianAPI()) {
         currentPage = 1
         self.recipesPublisher = recipesPublisher
@@ -21,5 +24,17 @@ class RecipesController: ObservableObject {
             .map { $0.map(RecipeViewModel.init) }
             .receive(on: RunLoop.main)
             .assign(to: &$recipes)
+    }
+
+    func loadMoreRecipes() {
+        currentPage += 1
+        recipesPublisher
+            .getLatestRecipes(currentPage: currentPage)
+            .map { $0.map(RecipeViewModel.init) }
+            .receive(on: RunLoop.main)
+            .sink { [weak self] in
+                self?.recipes.append(contentsOf: $0)
+            }
+            .store(in: &cancellable)
     }
 }
